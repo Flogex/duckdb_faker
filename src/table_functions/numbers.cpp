feb4450b99279eabedbe2eb4fe01ee1dd0533a81
Florian <florian@motherduck.com>
2024-12-01 14:01:30 +0100

    Return some actual random numbers

--- src/table_functions/numbers.cpp
+++ src/table_functions/numbers.cpp
@@ -1,4 +1,6 @@
 #include "numbers.hpp"
+#include "faker-cxx/number.h"
+#include <cstdint>
 
 namespace duckdb_faker {
 
@@ -15,13 +17,18 @@ unique_ptr<FunctionData> RandomIntBind(ClientContext &, TableFunctionBindInput &
 
 OperatorResultType RandomIntFunction(ExecutionContext &, TableFunctionInput &input, DataChunk &,
                                      DataChunk &output) {
+    D_ASSERT(output.ColumnCount() == 1);
     auto &state = input.global_state->Cast<RandomIntGlobalState>();
 
     if (state.is_finished) {
         output.SetCardinality(0);
     } else {
-        output.SetValue(0, 0, Value(42));
-        output.SetCardinality(1);
+        const idx_t cardinality = 10;
+        output.SetCardinality(cardinality);
+        for (idx_t row = 0; row < cardinality; row++) {
+            const int32_t random_num = faker::number::integer(0, 100);
+            output.SetValue(0, row, Value(random_num));
+        }
         state.is_finished = true;
     }
 
